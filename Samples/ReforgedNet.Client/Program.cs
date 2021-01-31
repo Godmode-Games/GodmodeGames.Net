3e74019a979f6aaf3f7bc0c072e0569554646b41
Manuel Kolm <kolm.manuel@gmx.at>
2021-01-31 19:41:57 +0100

    Added samples.

---
+++ Samples/ReforgedNet.Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using ReforgedNet.LL;
+using ReforgedNet.LL.Serialization;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReforgedNet.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Socket socket = new Socket(SocketType.Dgram, ProtocolType.Udp);
+            var settings = new RSocketSettings();
+            var remoteEndPoint = new IPEndPoint(IPAddress.Loopback, 7000);
+            var cancellationToken = new CancellationTokenSource();
+
+            RSocket rsocket = new RSocket(socket, settings, new RJsonSerialization(), new IPEndPoint(IPAddress.Any, 0), null, cancellationToken.Token);
+
+            // Register receiver
+            rsocket.RegisterReceiver(RSocket.DEFAULT_RECEIVER_ROUTE, (RNetMessage message) =>
+            {
+                Console.WriteLine($"Received from: {message.RemoteEndPoint} ,message:" + ASCIIEncoding.UTF8.GetString(message.Data));
+            });
+
+            Console.WriteLine("Client started.");
+
+            var data = ASCIIEncoding.UTF8.GetBytes("hello-server!");
+            rsocket.Send(RSocket.DEFAULT_RECEIVER_ROUTE, ref data, remoteEndPoint);
+
+            while (true)
+            {
+                rsocket.Dispatch();
+                await Task.Delay(100);
+            }
+
+            rsocket.Close();
+        }
+    }
+}
---
+++ Samples/ReforgedNet.Server/Program.cs
@@ -0,0 +1,50 @@
+﻿using ReforgedNet.LL;
+using ReforgedNet.LL.Serialization;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReforgedNet.Server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Socket socket = new Socket(SocketType.Dgram, ProtocolType.Udp);
+            var settings = new RSocketSettings();
+            var remoteEndPoint = new IPEndPoint(IPAddress.Any, 7000);
+            var cancellationToken = new CancellationTokenSource();
+            socket.Bind(remoteEndPoint);
+
+            RSocket rsocket = new RSocket(socket, settings, new RJsonSerialization(), remoteEndPoint, null, cancellationToken.Token);
+
+            // Register receiver
+            rsocket.RegisterReceiver(RSocket.DEFAULT_RECEIVER_ROUTE, async (RNetMessage message) =>
+            {
+                Console.WriteLine($"Received from: {message.RemoteEndPoint} ,message:" + ASCIIEncoding.UTF8.GetString(message.Data));
+
+                await Task.Delay(100);
+
+                var messageString = "hello-server!";
+                var data = ASCIIEncoding.UTF8.GetBytes(messageString);
+                rsocket.Send(1, ref data, message.RemoteEndPoint);
+
+                Console.WriteLine($"Sent message: {messageString}, to: {message.RemoteEndPoint}");
+            });
+
+            Console.WriteLine("Server started!");
+
+
+            while (true)
+            {
+                rsocket.Dispatch();
+                await Task.Delay(100);
+            }
+
+            rsocket.Close();
+        }
+    }
+}
